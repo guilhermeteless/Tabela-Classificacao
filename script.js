93fd5df653b64d18a034ace5747895d86d149381
Guilherme Teles <telesolvr@outlook.com>
2022-03-21 21:01:21 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// criando um objeto
 var loud = {posição: "1º", time:  "LOUD Academy", vitorias: 13, derrotas: 3};
 var miners = {posição: "2º", time: "Miners Academy", vitorias: 12, derrotas: 4};
 var paiN = {posição: "3º", time: "paiN Academy", vitorias: 10, derrotas: 6};
@@ -10,37 +11,50 @@ var kabum = {posição: "9º", time: "KaBuM! Academy", vitorias: 9, derrotas: 7}
 var rensga = {posição: "10º", time: "Rensga Academy", vitorias: 9, derrotas: 7};
 
 
-var time = [loud, miners, paiN, flamengo, furia, liberty, intz, red, kabum, rensga];
+// vamos criar uma LISTA com os times.
+var times = [loud, miners, paiN, flamengo, furia, liberty, intz, red, kabum, rensga];
 
-function exibeTimeNaTela(time){
-    var elemento = ""
-    for (var i = 0; i < time.length; i++) {
-          elemento += "<tr><td>" + time[i].posição + "</td>"
-          elemento += "<td>"+ time[i].time + "</td>"
-          elemento += "<td>"+ time[i].vitorias + "</td>"
-          elemento += "<td>"+ time[i].derrotas + "</td>"
-          elemento += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
-          elemento += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>"
-          elemento += "</tr>"
-    }
+//
+function exibeTimesNaTela(times) {
+  var elemento = "";
+  for (var i = 0; i < times.length; i++) {
+    //o += significa a mesma coisa que elemento = elemento + "<tr><td>times.nome</td>"
+    elemento += "<tr><td>" + times[i].posição + "</td>";
+    elemento += "<td>" + times[i].time + "</td>";
+    elemento += "<td>" + times[i].vitorias + "</td>";
+    elemento += "<td>" + times[i].derrotas + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento +=
+      "<td><button onClick='zerarPlacar(" + i + ")'>Zerar</button></td>";
+    elemento += "</tr>";
+  }
 
-    var tabelaTime = document.getElementById("tabelaTime");
-    tabelaTime.innerHTML = elemento;
+  var tabelaTimes = document.getElementById("tabelaTimes");
+  tabelaTimes.innerHTML = elemento;
 }
 
-exibeTimeNaTela(time);
+exibeTimesNaTela(times);
 
-
+// botão vitoria
 function adicionarVitoria(i) {
-    var time = time[i]
-    time.vitorias++
-    exibeTimeNaTela(time)
-
+  var time = times[i];
+  time.vitorias++;
+  exibeTimesNaTela(times);
 }
 
+// botão derrota
 function adicionarDerrota(i) {
-    var time = time[i]
-    time.derrotas++
-    exibeTimeNaTela(time)
-
+    var time = times[i];
+    time.derrotas++;
+    exibeTimesNaTela(times);
+}
+// botão zerar
+function zerarPlacar(i) {
+  var time = times[i];
+  time.vitorias = 0;
+  time.derrotas = 0;
+  exibeTimesNaTela(times);
 }
